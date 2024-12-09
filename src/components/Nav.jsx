6cb93d101958cf41f0f421c964202ade070353e1
CHISCO <brancketchisco@gmail.com>
2024-12-09 18:43:05 +0100

    Nav Cahnged - Purple

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ import { FaRegBell } from "react-icons/fa";
 
 export default function Nav() {
   return (
-    <div className="bg-red-950 text-white flex justify-end items-end text-end gap-10 h-20 py-4 px-5">
+    <div className="bg-purple-950 text-white flex justify-end items-end text-end gap-10 h-20 py-4 px-5">
         <div className="gap-2 flex justify-center items-center text-center border-l border-gray-300 pl-2 h-full">
             {/* <div className="borederr h-10  bg-gray-400"></div> */}
             <div className="flex gap-1 justify-center items-center ">
